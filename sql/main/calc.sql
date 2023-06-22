34c16e78366f723aa3654c3eee1bf4a9bcd9e9a9
bestialyokel <danil.37910@gmail.com>
2023-06-22 23:18:24 +0300

    Beeline DE school practice: replace redundant cte's with existing views

--- sql/main/calc.sql
+++ sql/main/calc.sql
@@ -152,25 +152,13 @@ with passengers as (
         contact_data
     from tickets
 ),
-flights_duration as (
-    select 
-    f.flight_id,
-    f.status,
-    case
-        when f.status = 'Arrived' then extract(epoch from (f.actual_arrival - f.actual_departure))
-        when f.status = 'Departed' then extract(epoch from (bookings.now() - f.actual_departure))
-        else 0
-    end as flight_duration
-    from flights f
-),
 passenger_flights_duration as (
     select 
     t.passenger_id,
-    sum(fd.flight_duration) as flights_duration_sum
-    from tickets t, ticket_flights tf, flights f, flights_duration fd
+    sum(f.actual_duration) as flights_duration_sum
+    from tickets t, ticket_flights tf, flights_v f
     where t.ticket_no = tf.ticket_no
     and tf.flight_id = f.flight_id
-    and fd.flight_id = f.flight_id
     group by t.passenger_id
 )
 insert into results(id, response)
@@ -193,55 +181,24 @@ order by city;
 -- 10
 
 -- 11
-with cities_directions as (
-    select
-    a1.city as lft_city, a2.city as rgt_city
-    from (
-        select
-        f.departure_airport, f.arrival_airport
-        from flights f
-        union all
-        select
-        f.arrival_airport, f.departure_airport
-        from flights f
-    ) x, airports a1, airports a2
-    where arrival_airport != departure_airport
-    and a1.airport_code = departure_airport and a2.airport_code = arrival_airport
-    group by a1.city, a2.city
-),
-citites_directions_count as (
-    select lft_city, count(*) as cnt from cities_directions fd 
-    group by lft_city
+with citites_directions_count as (
+    select arrival_city, count(*) as cnt from routes r 
+    group by arrival_city
 )
 insert into results(id, response)
-select 11, lft_city from citites_directions_count cdc
+select 11, arrival_city from citites_directions_count cdc
 where cnt = (select min(cnt) from citites_directions_count)
-order by lft_city;
+order by arrival_city;
 -- 11
 
 -- 12
 with cities as (
     select city from airports
     group by city
-),
-cities_directions as (
-    select
-    a1.city as lft_city, a2.city as rgt_city
-    from (
-        select
-        f.departure_airport, f.arrival_airport
-        from flights f
-        union all
-        select
-        f.arrival_airport, f.departure_airport
-        from flights f
-    ) x, airports a1, airports a2
-    where arrival_airport != departure_airport
-    and a1.airport_code = departure_airport and a2.airport_code = arrival_airport
-    group by a1.city, a2.city
 )
 insert into results(id, response)
-select 12, lft || '|' || rgt from (
+select 12, 
+    lft || '|' || rgt from (
     select distinct
     least    (lft_city, rgt_city) as lft,
     greatest (lft_city, rgt_city) as rgt
@@ -253,9 +210,9 @@ select 12, lft || '|' || rgt from (
         where c1.city != c2.city
         except
         (
-        select lft_city, rgt_city from cities_directions
+        select arrival_city, departure_city from routes group by arrival_city, departure_city
         union
-        select rgt_city, lft_city from cities_directions
+        select departure_city, arrival_city from routes group by departure_city, arrival_city
         )
     ) as x
 ) y
@@ -266,28 +223,13 @@ order by lft, rgt;
 with cities as (
     select city from airports
     group by city
-),
-cities_directions as (
-    select
-    a1.city as lft_city, a2.city as rgt_city
-    from (
-        select
-        f.departure_airport, f.arrival_airport
-        from flights f
-        union all
-        select
-        f.arrival_airport, f.departure_airport
-        from flights f
-    ) x, airports a1, airports a2
-    where a1.airport_code = departure_airport and a2.airport_code = arrival_airport
-    group by a1.city, a2.city
 )
 insert into results(id, response)
 select 13, city from (
-    select c2.city from cities c1, cities c2
-    where c1.city = 'Москва'
+    select city from cities
     except
-    select rgt_city from cities_directions where lft_city = 'Москва'
+    select arrival_city from routes where departure_city = 'Москва'
+    group by arrival_city
 ) x
 order by city;
 -- 13
@@ -326,41 +268,26 @@ where passenger_cnt = (select max(passenger_cnt) from test);
 -- 15
 
 -- 16
-with flights_duration as (
-    select 
-    f.flight_id,
-    f.status,
-    case
-        when f.status = 'Arrived' then extract(epoch from (f.actual_arrival - f.actual_departure))
-        when f.status = 'Departed' then extract(epoch from (bookings.now() - f.actual_departure))
-        else 0
-    end as flight_duration
-    from flights f
-)
 insert into results(id, response)
-select 16, (sum(extract(epoch from (f.scheduled_arrival - f.scheduled_departure))) - sum(fd.flight_duration))/60 as diff
-from flights f, flights_duration fd
-where f.flight_id = fd.flight_id 
-and f.status in ('Departed', 'Arrived');
+select 16, extract(epoch from (sum(f.scheduled_arrival - f.scheduled_departure) - sum(f.actual_duration)))/60 as diff
+from flights_v f
+where f.status in ('Departed', 'Arrived');
 -- 16
 
 -- 17
 insert into results(id, response)
-select 17, to_city from (
-    select a1.city as from_city, a2.city as to_city,
-    actual_departure,
-    case
-        when f.status = 'Arrived' then actual_arrival
-        when f.status = 'Departed' then bookings.now()
-    end as flight_end_date
-    from flights f, airports a1, airports a2
-    where f.departure_airport = a1.airport_code
-    and f.arrival_airport = a2.airport_code
+select 17, arrival_city from (
+    select 
+        f.departure_city, 
+        f.arrival_city,
+        f.actual_departure,
+        f.actual_duration
+    from flights_v f
 ) x
-where from_city = 'Санкт-Петербург'
-and '2016-09-13' between actual_departure and flight_end_date
-group by from_city, to_city
-order by to_city;
+where departure_city = 'Санкт-Петербург'
+and '2016-09-13' between actual_departure and actual_departure + actual_duration 
+group by departure_city, arrival_city
+order by arrival_city;
 -- 17
 
 -- 18
@@ -394,9 +321,8 @@ select 20, COALESCE(avg(flights_cnt), 0) from (
     select
     extract(day from f.actual_departure) as day,
     count(*) as flights_cnt
-    from flights f, airports a
-    where f.departure_airport = a.airport_code
-    and a.city = 'Москва'
+    from flights_v f
+    where f.departure_city = 'Москва'
     and status in ('Departed', 'Arrived')
     and extract(year from f.actual_departure) = 2016
     and extract(month from f.actual_departure) = 9
@@ -405,28 +331,18 @@ select 20, COALESCE(avg(flights_cnt), 0) from (
 -- 20
 
 -- 21
-with flights_duration as (
-    select 
-    f.flight_id,
-    f.status,
-    case
-        when f.status = 'Arrived' then extract(epoch from (f.actual_arrival - f.actual_departure))
-        when f.status = 'Departed' then extract(epoch from (bookings.now() - f.actual_departure))
-        else 0
-    end as flight_duration
-    from flights f
-)
 insert into results(id, response)
-select 21, city from (
-    select 
-    a.city,
-    avg(fd.flight_duration) as avg_flight_duration
-    from flights f, airports a, flights_duration fd
-    where f.departure_airport = a.airport_code
-    and fd.flight_id = f.flight_id
-    group by a.city
+select 21, departure_city from (
+        select departure_city from (
+        select 
+        departure_city,
+        avg(f.actual_duration) as avg_flight_duration
+        from flights_v f
+        group by departure_city
+    ) x
+    where extract(epoch FROM avg_flight_duration)/3600 > 3
+    order by avg_flight_duration DESC
+    limit 5
 ) x
-where avg_flight_duration > 3
-order by avg_flight_duration DESC
-LIMIT 5;
+order by departure_city;
 -- 21
